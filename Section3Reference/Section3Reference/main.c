903a0b0011a428e1fb0b558f3108f8678633d652
wj <309307194@qq.com>
2020-06-27 14:20:16 +0800

    Test new computer

---
+++ Section3Reference/Section3Reference/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Section3Reference
+//
+//  Created by niwodiy on 2020/6/27.
+//  Copyright © 2020 niwodiy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
